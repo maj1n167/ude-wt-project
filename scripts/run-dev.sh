6c66730adf66e197d923ae7722b233fc9823bd62
Oguzhan Kaluk <oguzhan.kaluk@gmail.com>
2024-10-09 15:37:22 +0200

    initialize webstorm

---
+++ scripts/run-dev.sh
@@ -0,0 +1,5 @@
+# WIP
+# looking for a way to have one dev runner,
+# who launches mongodb, backend and frontend.
+# backend and frontend should be hotswapping
+# when finished, add to run configurations
