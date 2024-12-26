6290788b5d1ff270945ca93fc30cae4b97427fef
Oguzhan Kaluk <oguzhan.kaluk@gmail.com>
2024-12-26 13:22:34 +0100

    #1 add home + navigation bar elements

--- client/src/components/home/home.component.ts
+++ client/src/components/home/home.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import {SharedMaterialDesignModule} from '../../module/shared-material-design/shared-material-design.module';
+import { SharedMaterialDesignModule } from '../../module/shared-material-design/shared-material-design.module';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    SharedMaterialDesignModule
-  ],
+  imports: [SharedMaterialDesignModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent { }
+export class HomeComponent {}
--- client/src/components/navigation-bar/navigation-bar.component.ts
+++ client/src/components/navigation-bar/navigation-bar.component.ts
@@ -1,15 +1,15 @@
 // Angular modules
-import {Component, inject} from '@angular/core';
+import { Component, inject } from '@angular/core';
 
 // Custom modules
 import { SharedAntDesignModule } from '../../module/shared-ant-design/shared-ant-design.module';
-import {Router, RouterLink} from '@angular/router';
-import {SharedMaterialDesignModule} from '../../module/shared-material-design/shared-material-design.module';
-import {MatHeaderRow} from '@angular/material/table';
-import {MatMenu, MatMenuItem} from '@angular/material/menu';
-import {MatTabLabel, MatTabLink, MatTabNav} from '@angular/material/tabs';
-import {NzMenuDirective, NzMenuItemComponent} from 'ng-zorro-antd/menu';
-import {NzTypographyComponent} from 'ng-zorro-antd/typography';
+import { Router, RouterLink } from '@angular/router';
+import { SharedMaterialDesignModule } from '../../module/shared-material-design/shared-material-design.module';
+import { MatHeaderRow } from '@angular/material/table';
+import { MatMenu, MatMenuItem } from '@angular/material/menu';
+import { MatTabLabel, MatTabLink, MatTabNav } from '@angular/material/tabs';
+import { NzMenuDirective, NzMenuItemComponent } from 'ng-zorro-antd/menu';
+import { NzTypographyComponent } from 'ng-zorro-antd/typography';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -26,7 +26,7 @@ import {NzTypographyComponent} from 'ng-zorro-antd/typography';
     NzMenuDirective,
     NzMenuItemComponent,
     RouterLink,
-    NzTypographyComponent
+    NzTypographyComponent,
   ],
   templateUrl: './navigation-bar.component.html',
   styleUrl: './navigation-bar.component.css',
