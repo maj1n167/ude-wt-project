3ba527c6d04ac4f5962188bb9d130f8073d500af
N777W <nickwald7777@gmail.com>
2024-12-28 18:35:56 +0100

    #3 Merged main

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../components/home/home.component';
+import { TrainingSessionComponent } from '../components/training-session/training-session.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'training', component: TrainingSessionComponent },
   { path: '**', redirectTo: '' },
 ];
