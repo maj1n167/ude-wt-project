07acdd047f4b6e03999a9c28490409c083cdf56b
Oguzhan Kaluk <oguzhan.kaluk@gmail.com>
2024-12-28 23:21:50 +0100

    #9 extend stack with private and creator field

--- server/src/models/stack-model.js
+++ server/src/models/stack-model.js
@@ -6,6 +6,16 @@ const stackSchema = new Schema({
   name: {
     type: String,
     required: true,
+    unique: true,
+  },
+  private: {
+    type: Boolean,
+    default: true,
+  },
+  creator: {
+    type: ObjectId,
+    ref: "User",
+    required: false, // todo: change to true when user auth is implemented
   },
 });
 
