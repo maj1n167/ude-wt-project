d6aa53cf2039e7480fc205e7db25de6909d915fe
N777W <nickwald7777@gmail.com>
2024-12-28 18:55:31 +0100

    #3 used pages instead of app routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../components/home/home.component';
-import { TrainingSessionComponent } from '../components/training-session/training-session.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'training', component: TrainingSessionComponent },
   { path: '**', redirectTo: '' },
 ];
--- client/src/pages/pages.routes.ts
+++ client/src/pages/pages.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { StacksComponent } from './stacks/stacks.component';
 import { CardsComponent } from './cards/cards.component';
+import { TrainingSessionComponent } from '../components/training-session/training-session.component';
 
 export const pagesRoutes: Routes = [
   { path: 'stacks', component: StacksComponent },
   { path: 'cards/:stackId', component: CardsComponent },
+  { path: 'training', component: TrainingSessionComponent },
 ];
