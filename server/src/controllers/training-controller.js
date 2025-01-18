62381a6235bd6500b8b3cf5ad0d99e1c7647c5d2
Oguzhan Kaluk <oguzhan.kaluk@gmail.com>
2025-01-18 20:56:17 +0100

    #11 current state of training controller. starting training should work

--- server/src/controllers/training-controller.js
+++ server/src/controllers/training-controller.js
@@ -1,14 +1,34 @@
 const Card = require("../models/card-model");
 const Stack = require("../models/stack-model");
 const Training = require("../models/training-model");
-const auth = require("../middlewares/auth-middleware");
 
 exports.getTrainingStatus = async (req, res) => {
   /**
    * This function returns for the current user the following information:
    * - A list of all stacks that the user has started training on
-   * - For each stack, the progress of the training ( 0, 1, 2, 3, 4, 5)
+   * - For each stack, the progress of the training ( 0, 1, 2, 3)
    */
+  Training.distinct("stackId", { userId: req.user._id }, (err, stackIds) => {
+    if (err) {
+      console.log("training-controller, ln14", err);
+    }
+    const stackProgress = {};
+    stackIds.forEach((stackId) => {
+      Training.find(
+        { userId: req.user._id, stackId: stackId },
+        (err, trainings) => {
+          if (err) {
+            console.log("training-controller, ln20", err);
+          }
+          stackProgress[stackId] =
+            trainings.reduce((acc, training) => {
+              return acc + training.rating;
+            }, 0) / (trainings.length || 1);
+        },
+      );
+    });
+    return res.status(200).json(stackProgress);
+  });
 };
 
 exports.startTraining = async (req, res) => {
@@ -18,6 +38,42 @@ exports.startTraining = async (req, res) => {
    * It should return the first 10 cards the user should train now.
    * the Cards are sorted if possible by the last time they were trained.
    */
+
+  const stackId = req.body.stackId;
+  const checkStackForUser = await Training.findOne({
+    userId: req.user._id,
+    stackId: stackId,
+  });
+  if (!checkStackForUser) {
+    this.addStackForUser(req.user._id, stackId);
+  }
+  // get all cards of current user and stackid provided
+  Training.find(
+    { userId: req.user._id, stackId: stackId },
+    (err, trainingCards) => {
+      if (err) {
+        console.log(err);
+      }
+      // sort the cards by rating and get the first 10
+      trainingCards
+        .sort((a, b) => {
+          return a.rating - b.rating;
+        })
+        .slice(0, 10)
+        .map((trainingCard) => {
+          // get the card for the cards that are gonna be trained
+          const cards = [];
+          Card.findById(trainingCard.cardId, (err, card) => {
+            if (err) {
+              console.log(err);
+            }
+            cards.push(card);
+          });
+          // return the cards that are to be trained
+          return res.status(200).json(cards);
+        });
+    },
+  );
 };
 
 exports.finishTraining = async (req, res) => {
@@ -28,6 +84,28 @@ exports.finishTraining = async (req, res) => {
 };
 
 // stack section
+exports.addStackForUser = async (userId, stackId) => {
+  Card.find({ stackId: stackId }, (err, cards) => {
+    if (err) {
+      console.log(err);
+    }
+    cards.forEach((card) => {
+      Training.create(
+        {
+          userId: userId,
+          stackId: stackId,
+          cardId: card._id,
+        },
+        (err) => {
+          if (err) {
+            console.log(err);
+          }
+        },
+      );
+    });
+  });
+};
+
 exports.deleteStack = async (stackId) => {
   /**
    * This function receives a stack that has been deleted.
